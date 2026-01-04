e41edc72651ae16a82cda3db98c27dbfe53e63a1
Mahereidd <52230954@students.liu.edu.lb>
2026-01-04 20:21:33 +0200

    bugs

--- src/App.js
+++ src/App.js
@@ -15,21 +15,23 @@ export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <BrowserRouter>
+        <BrowserRouter style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
           <Navbar />
 
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/add-products" element={<AddProducts />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/iphonedetails/:model" element={<IPhoneDetails />} />
-          </Routes>
+          <div style={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/add-products" element={<AddProducts />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/iphonedetails/:model" element={<IPhoneDetails />} />
+            </Routes>
+          </div>
 
-          <Footer /> 
+          <Footer />
         </BrowserRouter>
       </CartProvider>
     </AuthProvider>
